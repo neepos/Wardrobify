ef9ca16a16623e908cf6253babc1e03d3afa639b
Mac <macstephens@gmail.com>
2023-06-01 13:02:51 -0400

    Git setup, made shoe model, need to make binVO model and make migrations

--- shoes/api/shoes_project/settings.py
+++ shoes/api/shoes_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "shoes_api.apps.ShoesAppConfig",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=150)
+    color = models.Charfield(max_length=150)
+    picture_url = models.URLField(null=True)
+    
+    # add the bin that the shoe is stored in
+    # remember that the bin is the VO, so figure that out
+    bin = models.ForeignKey(
+        "BinVO",
+        related_name="shoes",
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+
+    def __str__(self):
+        return self.name
+
+# make the bin VO
